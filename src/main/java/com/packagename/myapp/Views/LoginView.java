7dc656d7e89853d834002762a22b76104c195969
Yandry <yandrypinargote@gmail.com>
2020-03-23 11:28:01 -0500

    modo prueba

--- src/main/java/com/packagename/myapp/Views/LoginView.java
+++ src/main/java/com/packagename/myapp/Views/LoginView.java
@@ -1,63 +1,73 @@
 package com.packagename.myapp.Views;
 
-import com.packagename.myapp.MainLayout;
-import com.vaadin.flow.component.login.LoginI18n;
-import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.*;
-import com.vaadin.flow.server.VaadinSession;
+
+import javax.sound.sampled.Port;
+
 
 @Route("login")
 @PageTitle("AcademyTic")
-public class LoginView extends LoginOverlay implements AfterNavigationObserver, BeforeEnterObserver {
-    boolean isAuthenticated = false;
+public class LoginView extends HorizontalLayout {
     public LoginView(){
-
-        LoginI18n viewLogin = LoginI18n.createDefault();
-        viewLogin.setHeader(new LoginI18n.Header());
-        viewLogin.getHeader().setTitle("AcademyTic");
-        viewLogin.getHeader().setDescription("Sistema de gestión académico y admistrativo");
-        viewLogin.getForm().setUsername("Usuario");
-        viewLogin.getForm().setTitle("Iniciar sesión");
-        viewLogin.getForm().setPassword("Clave");
-        viewLogin.getForm().setSubmit("Iniciar sesión");
-        viewLogin.getForm().setForgotPassword("Olvidé mi clave");
-        viewLogin.getErrorMessage().setTitle("Usuario/clave inválido");
-        viewLogin.getErrorMessage().setMessage("Revise el usuario o clave e intente nuevamente");
-        viewLogin.setAdditionalInformation("Derechos reservados Unidad Educativa Julia Navarrete Mendoza 2020");
-
-        setForgotPasswordButtonVisible(false);
-        setI18n(viewLogin);
-        setAction("inicio");
-        /*addLoginListener(e->{
-            authenticate(e);
-        });*/
-
-    }
-   /* private boolean authenticate(LoginEvent e) {
-
-        if(LoginController.login(e.getUsername(),e.getPassword())) {
-            return true;
-        }else
-            setError(true);
-        setEnabled(true);
-        return false;
-    }*/
-
-    @Override
-    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-        if (VaadinSession.getCurrent().getAttribute("LOGIN")!=null) {
-            System.out.println("before true");
-            beforeEnterEvent.forwardTo(MainLayout.class);
-        } else {
-            System.out.println("before false");
-            setOpened(true);
-        }
+        setPadding(false);
+        setSpacing(false);
+        addClassName("login");
+        setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        add(buildPortada(), buildFormLogin());
     }
 
-    @Override
-    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
-        setError(afterNavigationEvent.getLocation().getQueryParameters().getParameters().containsKey(
-                "error"));
+    private Component buildFormLogin() {
+        VerticalLayout FormLogin = new VerticalLayout();
+        H2 title = new H2("LOGIN");
+        H3 nombre = new H3("USUARIO");
+        TextField usuario = new TextField("Ingrese su nombre de usuario:");
+        usuario.setWidthFull();
+        usuario.setPrefixComponent(new Icon(VaadinIcon.USER));
+        PasswordField clave = new PasswordField("Ingrese su contraseña:");
+        clave.setWidthFull();
+        Button next = new Button("Siguiente");
+        next.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        next.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_RIGHT));
+        Button next2 = new Button("Siguiente");
+        next2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        next2.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_RIGHT));
+        FormLogin.add(title,usuario,next);
+        FormLogin.setAlignItems(Alignment.CENTER);
+        next.addClickListener(e->{
+            FormLogin.removeAll();
+            FormLogin.add(title, nombre, clave, next2 );
+        });
+        return FormLogin;
+    }
+
+    private Component buildPortada() {
+        VerticalLayout Portada = new VerticalLayout();
+        Portada.setPadding(false);
+        Portada.setSizeFull();
+        Portada.setAlignItems(Alignment.CENTER);
+        Portada.addClassName("login-portada");
+        Image logo = new Image("img/escudo.png","escudo");
+        logo.addClassName("login-logo");
+        H2 title = new H2("UE JUNAME");
+        H4 title2 = new H4("Derechos Reservados");
+        Portada.add(logo, title, title2);
+        Portada.setJustifyContentMode(JustifyContentMode.CENTER);
+        return Portada;
     }
 
 
